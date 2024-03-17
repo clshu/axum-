bd0ebf46c14adbd792b9906ae1c686be39f85337
clshu <clshu2020@gmail.com>
2024-03-17 17:39:32 +0800

    Create a hello world listener: 1. Add crates 'axum', 'tokio', 'anyhow'. 2. Create a handler and use axum to bind to socket. 3. axum's serving the handler on the socket method has been changed. Read documents to find the new way.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)] // For beginning only.
+
+// use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
+use axum::response::Html;
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route(
+        "/",
+        get(|| async { Html("Hello, <strong> World! </string>") }),
+    );
+
+    let addr_port = "0.0.0.0:3000";
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind(addr_port).await.unwrap();
+    println!("->> LISTENING on {addr_port}\n");
+    axum::serve(listener, app).await.unwrap();
 }
