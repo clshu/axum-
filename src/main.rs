96ae81cebd14725f06c73c3f4dfa87940c5a436d
clshu <clshu2020@gmail.com>
2024-03-17 18:16:14 +0800

    Creaet a handler_hello to handle /hello route

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,27 @@
 
 // use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
-use axum::response::Html;
+use axum::response::{Html, IntoResponse};
 use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route(
-        "/hello",
-        get(|| async { Html("Hello, <strong> World! </string>") }),
-    );
+    let app = Router::new().route("/hello", get(handler_hello));
 
     let addr_port = "0.0.0.0:3000";
 
     let listener = tokio::net::TcpListener::bind(addr_port).await.unwrap();
     println!("->> LISTENING on {addr_port}\n");
     axum::serve(listener, app).await.unwrap();
+
+    // Handler start
+
+    async fn handler_hello() -> impl IntoResponse {
+        println!("->> {:<12} - hendler_hello", "HANDLER");
+
+        Html("Hello, <strong> World! </string> 2")
+    }
+
+    // Handler end
 }
