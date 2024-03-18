5eb1ba1c818f013ef3ea05b72570bcc876f8b57f
clshu <clshu2020@gmail.com>
2024-03-18 14:18:37 +0800

    Create a static route handler as the default handler. Add tower-http -F fs.

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,16 @@
 use axum::extract::{Path, Query};
 // use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use axum::response::{Html, IntoResponse};
+use axum::routing::get_service;
 use axum::{routing::get, Router};
 use serde::Deserialize;
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
-    let routes_all = Router::new().merge(routes_hello());
+    let routes_all = Router::new()
+        .merge(routes_hello())
+        .fallback_service(routes_static());
 
     let addr_port = "0.0.0.0:3000";
 
@@ -16,15 +20,23 @@ async fn main() {
     println!("->> LISTENING on {addr_port}\n");
     axum::serve(listener, routes_all).await.unwrap();
 
-    // Router start -- Route hello*
+    // Routes start -- Route hello*
     fn routes_hello() -> Router {
         Router::new()
             .route("/hello", get(handler_hello))
             .route("/hello2/:name", get(handler_hello2))
     }
-    // Router end
-    // Handler with extractor start
+    // -- Route static
+    // `cargo add tower-http -F fs` for ServeDir
+    fn routes_static() -> Router {
+        Router::new().nest_service("/", get_service(ServeDir::new("./")))
+    }
 
+    // Routes end
+
+    // Handler start
+
+    // -- extractor Query
     #[derive(Debug, Deserialize)]
     struct HelloParams {
         name: Option<String>,
@@ -38,6 +50,7 @@ async fn main() {
         Html(format!("Hello, <strong>{name}</string>"))
     }
 
+    // -- extractor Path
     // e.g., '/hello2/Mike
     async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
         println!("->> {:<12} - hendler_hello2 - {name:?}", "HANDLER");
--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -8,7 +8,9 @@ const ADDR: &str = "http://localhost:3000";
 async fn quick_dev() -> Result<()> {
     let hc = httpc_test::new_client(ADDR)?;
 
+    hc.do_get("/hello?name=Jen").await?.print().await?;
     hc.do_get("/hello2/Mike").await?.print().await?;
+    hc.do_get("/src/main.rs").await?.print().await?;
 
     Ok(())
 }
